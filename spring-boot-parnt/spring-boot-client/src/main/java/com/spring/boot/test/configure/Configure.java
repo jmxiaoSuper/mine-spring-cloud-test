34e05d8a3da7ad285ce976ed3babdd1eaf61310b
孤舟蓑笠翁 <jmxiao_9527@aliyun.com>
2019-05-08 15:14:31 +0800

    测试条件注解类 使用lambda表达式简化

--- spring-boot-parnt/spring-boot-client/src/main/java/com/spring/boot/test/configure/Configure.java
+++ spring-boot-parnt/spring-boot-client/src/main/java/com/spring/boot/test/configure/Configure.java
@@ -1,17 +1,14 @@
 package com.spring.boot.test.configure;
 
-import com.netflix.discovery.DiscoveryClient;
 import com.spring.boot.test.condition.LinuxConditionService;
 import com.spring.boot.test.condition.WindowsConditionService;
 import com.spring.boot.test.service.PrintService;
-import org.springframework.cloud.netflix.eureka.MutableDiscoveryClientOptionalArgs;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.BufferingClientHttpRequestFactory;
 import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
-
 import java.util.Collections;
 
 /**
@@ -30,31 +27,15 @@ public class Configure {
 		return restTemplate;
 	}
 
-//	@Bean
-//	public DiscoveryClient.DiscoveryClientOptionalArgs getArgs(){
-//		return new MutableDiscoveryClientOptionalArgs();
-//	}
-
-
 	@Bean
 	@Conditional(WindowsConditionService.class)
 	public PrintService getWindowPrintService() {
-		return new PrintService() {
-			@Override
-			public void print() {
-				System.out.println("this is windows");
-			}
-		};
+		return () -> System.out.println("this is windows");
 	}
 
 	@Bean
 	@Conditional(LinuxConditionService.class)
 	public PrintService getLinuxPrintService() {
-		return new PrintService() {
-			@Override
-			public void print() {
-				System.out.println("this is linux");
-			}
-		};
+		return () -> System.out.println("this is linux");
 	}
 }
